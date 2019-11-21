bfab4b381c0bed75ba23433780efd91814cabdb6
Simon Colman <s_colman@hotmail.com>
2019-11-21 23:02:25 +1100

    I have added a screen with a generate password the password displays as an alert I have tried to create a div element to display the password but is not displaying yet

--- script.js
+++ script.js
@@ -1,28 +1,70 @@
+var count = 0;
+var passwordEl = document.querySelector("#password");
+
+passwordEl.addEventListener("click", function () {
+    var numSymb = false;
+    var specialSymb = false;
+    var upperCase = false;
+
+    var passwordLength = prompt("Please select a number greater than 8 ?")
+    var passwordLengthNumber = parseInt(passwordLength)
+
+    if (passwordLengthNumber > 8) {
+        var numSymb = confirm("Would you like to have any numbers in your password ?");
+        var specialSymb = confirm("Would you like a special symbol in your password");
+        var upperCase = confirm(" Would you like to have an upper case letter in your password ?");
+
+        passwordGenerator(numSymb, specialSymb, upperCase)
+
+  
+    }
+    else {
+        alert("please refresh the page and enter another number higher than 8")
+    }
+
+    if (specialSymb) { }
+
+    else {
+        alert("please select to have a special symbole in the password")
+    }
+
+    if (upperCase) { }
+
+    else {
+        alert("please select to have an upper case letter")
+    }
+
+    if (numSymb) { }
+    else { alert("please select to have a number in your password") }
 
 
 
+    function passwordGenerator(shouldIncludespecialSymb, shouldIncludeUpperCase, shouldIncludeNumSymb) {
+        var result = specialSymb, upperCase, numSymb;
+        var characters = 'abcdefghijklmnopqesrtuvwxyz';
+        var charactersLength = passwordLength;
+        var numbers = '123456789';
+        var specialsymbol = '!@#$%^&*()<>?:"{}|-,./;'
 
 
-var passLength = Prompt("How long would you like your password ? Please select a number 8 - 128 characters ");
-var upperCase = Confirm(" Would you like to have an upper case letter in your password ?");
-var specialSymb = Confirm("Would you like a spcial symbol in your password");
-var  numSymb = Confirm("Would you like to have any numbers in your password ?");
-
-function generate {
-
-
-
-if passLength 
-
+        for (var i = 0; i < passwordLength; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        alert("your password is " + result);
+    }
 
     
-}
+
+    function displayPassword() {
+        var passDisplay = document.createElement("p");
+        var passDisplayWord = document.createTextNode("your password is" + result);
+        passDisplay.appendChild(passDisplayWord);
+        document.body.appendChild(passDisplay);
 
 
-
-
-
-
+    }
+        console.log(displayPassword)
+});
 
 
 
