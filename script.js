2a58c55a7a4fdb54c4a806abd3be67e2307611c9
Simon Colman <s_colman@hotmail.com>
2019-11-22 23:02:40 +1100

    included the special symbol function, need to understand why the password length is distorted

--- script.js
+++ script.js
@@ -1,69 +1,85 @@
 var count = 0;
 var passwordEl = document.querySelector("#password");
+var passContainer = document.getElementById("#passContainer")
+
 
 passwordEl.addEventListener("click", function () {
-    var numSymb = false;
-    var specialSymb = false;
-    var upperCase = false;
 
     var passwordLength = prompt("Please select a number greater than 8 ?")
     var passwordLengthNumber = parseInt(passwordLength)
 
     if (passwordLengthNumber > 8) {
-        var numSymb = confirm("Would you like to have any numbers in your password ?");
+        // var numSymb = confirm("Would you like to have any numbers in your password ?");
         var specialSymb = confirm("Would you like a special symbol in your password");
-        var upperCase = confirm(" Would you like to have an upper case letter in your password ?");
-
-        passwordGenerator(numSymb, specialSymb, upperCase)
+        // var upperCase = confirm(" Would you like to have an upper case letter in your password ?");
 
   
     }
     else {
-        alert("please refresh the page and enter another number higher than 8")
+        alert("please enter another number higher than 8")
     }
 
-    if (specialSymb) { }
+    if (specialSymb ===true) { passwordGenerator(specialSymb) 
+
+    }
 
     else {
-        alert("please select to have a special symbole in the password")
+        alert("please select to have a special symbol in the password")
     }
 
-    if (upperCase) { }
+    // if (upperCase) { }
 
-    else {
-        alert("please select to have an upper case letter")
-    }
+    // else {
+    //     alert("please select to have an upper case letter")
+    // }
 
-    if (numSymb) { }
-    else { alert("please select to have a number in your password") }
+    // if (numSymb) { }
+    // else { alert("please select to have a number in your password") }
 
 
-
-    function passwordGenerator(shouldIncludespecialSymb, shouldIncludeUpperCase, shouldIncludeNumSymb) {
-        var result = specialSymb, upperCase, numSymb;
+    
+    function passwordGenerator(shouldIncludeSpecialSymb) {
+   
+        var result = ''
         var characters = 'abcdefghijklmnopqesrtuvwxyz';
         var charactersLength = passwordLength;
-        var numbers = '123456789';
-        var specialsymbol = '!@#$%^&*()<>?:"{}|-,./;'
+        // var numbers = '123456789';
+        var specialSymb = '!@#$%^&*()<>?:"{}|-,./;' 
+        // var shouldIncludeSpecialSymb = '';
+        
+        // if (shouldIncludeSpecialSymb === true) {
+        //     characters *= specialSymb
+        // }
 
+        for(var i=0; i<passwordLength; i++){
+            result+= specialSymb.charAt(Math.floor(Math.random() * charactersLength));
+                        }
 
         for (var i = 0; i < passwordLength; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
+        
         alert("your password is " + result);
+    
+        displayPassword(result)
+    
     }
 
     
 
-    function displayPassword() {
+    function displayPassword(result) {
         var passDisplay = document.createElement("p");
-        var passDisplayWord = document.createTextNode("your password is" + result);
+        var passDisplayWord = document.createTextNode(
+            "your password is " + result);
         passDisplay.appendChild(passDisplayWord);
         document.body.appendChild(passDisplay);
 
 
+
     }
-        console.log(displayPassword)
+    // displayPassword()
+
+    // console.log(displayPassword)
 });
 
 
